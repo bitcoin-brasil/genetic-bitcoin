36cd4ad2209a4a13b42cef595c8e5ce474ef0a03
Bruno Garcia <brunoely.gc@gmail.com>
2021-06-05 11:42:43 -0300

    population: create the PoC for population creation

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from random import randrange
+from random import randrange, choice
+
+# Satoshis you want to spend
+VALUE = 1000
+
+# Satoshis you want to pay in fees
+FEES = 100
+
+UTXOS_VALUES = [1, 123, 1432, 12, 21]
 
 POPULATION_SIZE = 5
 
@@ -9,13 +17,20 @@ def create_population(utxos_values):
     
     for _ in range(0, POPULATION_SIZE):
         genes_num = randrange(1, utxos_size)
-        for _ in range(0, genes_num):
-            individual = []
-            individual.push()
-
-
-
+        individual = []
+        choice_value = 0
+        for _ in range(genes_num):
+            while True:
+                choice_value = choice(utxos_values)
+                if choice_value in individual:
+                    choice_value = choice(utxos_values)
+                else:
+                    individual.append(choice_value)
+                    break
+        POPULATION.append(individual)
+    
+    
 def genetic_algorithm():
-    create_population([1, 123, 1432, 12, 21])
+    create_population(UTXOS_VALUES)
 
 genetic_algorithm()
